46e25ac356010676706e321c8db1f94dca4995ae
unkabas <undercannabis10@gmail.com>
2024-10-16 23:36:49 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
-import Main from './main/page'
-import Auth from './signin/page'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AddSong from './components/addSong/addSong'
+import Card from './components/card/card'
+import Navbar from './components/nav/navbar'
+import SignIn from './signin/page'
 
-function App() {
+export default function App() {
 	return (
-		<div>
-			<Auth />
-			<Main />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<Navbar />
+							<h1 className='font-extrabold text-3xl text-[#D9D9D9] p-5'>
+								ROOM'S PLAYLIST
+							</h1>
+							<div className='grid grid-cols-3 pl-5'>
+								<Card
+									image='https://picsum.photos/200'
+									artist='Dua Lipa'
+									song='Physical'
+								/>
+							</div>
+							<h1 className='font-extrabold text-3xl text-[#D9D9D9] p-5 mt-10'>
+								SONGS
+							</h1>
+							<div className='flex justify-center gap-5'>
+								<div className='mx-5 flex items-center justify-center'>
+									<AddSong />
+								</div>
+								<Card
+									image='https://picsum.photos/200'
+									artist='Dua Lipa'
+									song='Physical'
+								/>
+								<Card
+									image='https://picsum.photos/200'
+									artist='Dua Lipa'
+									song='Physical'
+								/>
+								<Card
+									image='https://picsum.photos/200'
+									artist='Dua Lipa'
+									song='Physical'
+								/>
+							</div>
+						</>
+					}
+				/>
+				<Route path='/signin' element={<SignIn />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import ReactDOM from 'react-dom'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-import Main from './main/page'
-import SignIn from './signin/page'
 
-ReactDOM.render(
-	<Router>
-		<Routes>
-			<Route path='/main' element={<Main />} />
-			<Route path='/signin' element={<SignIn />} />
-		</Routes>
-	</Router>,
-	document.getElementById('root')
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 )
